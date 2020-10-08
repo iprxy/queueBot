a5e646d7a2ec574bf0b3c205ff7a9d2942eefec0
Artem Kulikov <i@iprxy.me>
2020-10-08 13:16:20 +0300

    remove lodash

--- utils/getKeyboard.js
+++ utils/getKeyboard.js
@@ -1,6 +1,8 @@
-const _ = require('lodash')
-
 module.exports = getKeyboard = (array, type, username) => {
+  const chunk = (arr, size) =>
+    Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+      arr.slice(i * size, i * size + size)
+    )
   const keyboardButtons = array.map(item => {
     return {
       text: item,
@@ -9,7 +11,7 @@ module.exports = getKeyboard = (array, type, username) => {
   })
   const options = {
     reply_markup: JSON.stringify({
-      inline_keyboard: _.chunk(keyboardButtons, 5)
+      inline_keyboard: chunk(keyboardButtons, 5)
     })
   }
   return options
